b9eddbbeac1cca3afd61718609e6f0393de24ace
Marek Sklodowski <masklod@gmail.com>
2012-08-11 22:37:48 +0200

    Pong mostly done

--- MetroRetro/Games/Pong.cs
+++ MetroRetro/Games/Pong.cs
@@ -30,6 +30,8 @@ namespace MetroRetro.Games
         private Point _ballDir;
         private float _ballSpd;
 
+        private const float _ballSpdInc = 0.01f;
+
         private readonly Point _padSize = new Point(0.05f, 0.25f);
         private readonly Point _ballSize = new Point(0.01f, 0.01f);
 
@@ -40,7 +42,7 @@ namespace MetroRetro.Games
             _ballPos = new Point(0.5f, 0.5f);
 
             _playerSpd = 0.8f;
-            _enemySpd = 0.8f;
+            _enemySpd = 0.3f;
             _ballSpd = 0.3f;
 
             _playerDir = new Point(0.0f, 0.0f);
@@ -54,11 +56,11 @@ namespace MetroRetro.Games
 
         public override void Update(DeviceContext context, TargetBase target, Point screenSize, float dt, float elapsedTime)
         {
-            // Player moves
+            // Player moving
             _playerPos = _playerPos.Add(_playerDir.Mul(_playerSpd).Mul(dt)).Clamp(GamesParams.Margin0.Add(_padSize.Half()),
                                                                                   GamesParams.Margin1.Sub(_padSize.Half()));
-
-            // Ball moves
+                 
+            // Ball moving
             _ballPos = _ballPos.Add(_ballDir.Mul(_ballSpd).Mul(dt));
 
             // Ball collision with borders
@@ -73,14 +75,43 @@ namespace MetroRetro.Games
 
             // Ball collision with player pad
             if (_ballPos.IsInside(_playerPos.Sub(_padSize.Half()).Sub(_ballSize.Half()),
-                                   _playerPos.Add(_padSize.Half()).Add(_ballSize.Half())))
+                                  _playerPos.Add(_padSize.Half()).Add(_ballSize.Half())))
             {
                 _ballDir.X = 1;
                 _ballDir.Y = _ballPos.Sub(_playerPos).Y / _padSize.Half().Y;
                 _ballDir = _ballDir.Normalise();
+
+                _ballSpd += _ballSpdInc;
             }
 
+            // Ball collision with enemy pad
+            if (_ballPos.IsInside(_enemyPos.Sub(_padSize.Half()).Sub(_ballSize.Half()),
+                                  _enemyPos.Add(_padSize.Half()).Add(_ballSize.Half())))
+            {
+                _ballDir.X = -1;
+                _ballDir.Y = _ballPos.Sub(_enemyPos).Y / _padSize.Half().Y;
+                _ballDir = _ballDir.Normalise();
 
+                _ballSpd += _ballSpdInc;
+            }
+
+            // Enemy moving
+            if (Math.Abs(_ballPos.Y - _enemyPos.Y) <_padSize.Div(3).Y)
+            {
+                _enemyDir.Y = 0;
+            }
+            else 
+            {
+                if (_ballPos.Y > _enemyPos.Y)
+                    _enemyDir.Y = 1;
+                else
+                    _enemyDir.Y = -1;
+
+                _enemyPos = _enemyPos.Add(_enemyDir.Mul(_enemySpd).Mul(dt)).Clamp(GamesParams.Margin0.Add(_padSize.Half()),
+                                                                                  GamesParams.Margin1.Sub(_padSize.Half()));
+            }
+
+            // Drawing
             var playerBox = _playerPos.ToBox(_padSize);
             var enemyBox = _enemyPos.ToBox(_padSize);
             var ballBox = _ballPos.ToBox(_ballSize);
