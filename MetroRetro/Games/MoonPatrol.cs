9868890bcdb598d4d1712be1d3ba570c65ef6084
Marek Sklodowski <masklod@gmail.com>
2012-08-27 23:00:46 +0200

    Moon patrol - mostly done

--- MetroRetro/Games/MoonPatrol.cs
+++ MetroRetro/Games/MoonPatrol.cs
@@ -12,13 +12,21 @@ namespace MetroRetro.Games
         private Point _holePos;
         private readonly Point _holeDir = new Point(-1, 0);
         private float _holeSpd;
+        private bool _holeIsStone;
 
         private const float GroundY = 0.7f;
+        private const float JumpHeight = 0.2f;
+        private const float JumpSpeed = 2.0f;
+        private const float HoleSpdAdd = 0.03f;
 
         private readonly Point _playerSize = new Point(0.1f, 0.06f);
-        private readonly Point _holeSize = new Point(0.06f, 0.08f);
+        private Point _holeSize = new Point(0.06f, 0.08f);
 
-        
+
+        private Random _r = new Random();
+        private float _timeToNextPossibleJump;
+        private float _jumpTimer;
+
         public MoonPatrol(GameManager gameManager, float maxTime)
             : base(gameManager, maxTime)
         {
@@ -32,31 +40,53 @@ namespace MetroRetro.Games
         public override void NewGame()
         {
             _playerPos = new Point(GamesParams.MarginX0 + 0.2f, GroundY - _playerSize.Y / 2);
-            _holePos = new Point(GamesParams.MarginX0 + 0.8f, GroundY + _holeSize.Y / 2.5f);
 
+            AddNewObstacle();
+            _holePos.X = GamesParams.MarginX1 + _holeSize.X;
+
+            _jumpTimer = -1;
             _holeSpd = 0.2f;
+            _timeToNextPossibleJump = 0;
 
             base.NewGame();
         }
 
-        Random _r = new Random();
         public override void Update(DeviceContext context, TargetBase target, DeviceManager deviceManager, Point screenSize, float dt, float elapsedTime)
         {
             if (AfterStartFreeze)
             {
+                _timeToNextPossibleJump -= dt;
+
+                if (_jumpTimer >= 0)
+                {
+                    _jumpTimer += dt * JumpSpeed;
+                    _playerPos.Y = GroundY - _playerSize.Y / 2 - (float)Math.Sin(_jumpTimer) * JumpHeight;
+
+                    if (_jumpTimer > Math.PI)
+                    {
+                        _playerPos.Y = GroundY - _playerSize.Y / 2;
+                        _jumpTimer = -1;
+                        _gameManager.AddPoints(200);
+                        _gameManager.Interrupt();
+                    }
+                }
+
+                Debug(_jumpTimer.ToString());
+
                 // Hole moving
                 _holePos = _holePos.Add(_holeDir.Mul(_holeSpd).Mul(dt));
                 if (_holePos.X + _holeSize.X / 2 <= GamesParams.MarginX0)
                 {
-                    _holePos.X = (float)_r.NextDouble() + 1.0f;
+                    AddNewObstacle();
                 }
 
                 // Ball collision with player pad
                 if (_holePos.IsInside(_playerPos.Sub(_playerSize.Half()).Sub(_holeSize.Half()),
                                       _playerPos.Add(_playerSize.Half()).Add(_holeSize.Half())))
                 {
-               //     _gameManager.Die();
-               //     NewGame();
+                    _gameManager.Die();
+                    NewGame();
+                    _gameManager.Interrupt();
                 }
             }
 
@@ -72,12 +102,29 @@ namespace MetroRetro.Games
                 if (holeBox.Right > GamesParams.MarginX1)
                     holeBox.Right = GamesParams.MarginX1;
 
-                context.FillRectangle(screenSize.ApplyTo(holeBox), GamesParams.BackgroundColor);
+                context.FillRectangle(screenSize.ApplyTo(holeBox), _holeIsStone ? GamesParams.ObstaclesColor : GamesParams.BackgroundColor);
             }
             var playerBox = _playerPos.ToBox(_playerSize);
             context.FillRectangle(screenSize.ApplyTo(playerBox), GamesParams.PlayerColor);
 
             base.Update(context, target, deviceManager, screenSize, dt, elapsedTime);
+        }
+
+        private void AddNewObstacle()
+        {
+            _holeIsStone = _r.Next(1) == 0;
+            if (_holeIsStone)
+            {
+                _holeSize = new Point(0.02f + (float)_r.NextDouble() * 0.05f, 0.02f + (float)_r.NextDouble() * 0.05f);
+                _holePos = new Point((float)_r.NextDouble() / 2 + 1.2f, GroundY - _holeSize.Y / 2.1f);
+            }
+            else
+            {
+                _holeSize = new Point(0.07f + (float)_r.NextDouble() * 0.1f, 0.07f + (float)_r.NextDouble() * 0.1f);
+                _holePos = new Point((float)_r.NextDouble() / 2 + 1.2f, GroundY + _holeSize.Y / 2.1f);
+            }
+
+            _holeSpd += HoleSpdAdd;
         }
 
         public override void KeyPressed(InputType key)
@@ -91,6 +138,13 @@ namespace MetroRetro.Games
         {
             switch (key)
             {
+                case InputType.Up:
+                    if (_timeToNextPossibleJump > 0)
+                        break;
+
+                    _jumpTimer = 0.0f;
+
+                    break;
             }
         }
     }
